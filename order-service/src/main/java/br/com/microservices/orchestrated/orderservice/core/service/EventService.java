66ed2b196767e3f089eceb2c9bf6c1618acdb731
RodofoMeneguetti <paulo.meneguettitec@gmail.com>
2023-10-23 19:02:30 -0300

    publicado envio de evento, e criado principais modal de product

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -1,15 +1,62 @@
 package br.com.microservices.orchestrated.orderservice.core.service;
 
+import br.com.microservices.orchestrated.orderservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import br.com.microservices.orchestrated.orderservice.core.dto.EventFilters;
 import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static io.micrometer.common.util.StringUtils.isEmpty;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class EventService {
 
     private final EventRepository repository;
+
+    public void notifyEnding(Event event) {
+        event.setOrderId(event.getOrderId());
+        event.setCreatedAt(LocalDateTime.now());
+        save(event);
+        log.info("Order {} with saga notified! TransactionID: {}", event.getOrderId(), event.getTransactionId());
+    }
+
+    public List<Event> findAll() {
+        return repository.findAllByOrderByCreatedAtDesc();
+    }
+
+    public Event findByFilters(EventFilters filters) {
+        validationEmptyFilters(filters);
+        if (!isEmpty(filters.getOrderId())) {
+            return findByOrderId(filters.getOrderId());
+        } else {
+            return findByTransactionId(filters.getTransactionId());
+        }
+    }
+
+    private Event findByOrderId(String orderId) {
+        return repository
+                .findTop1ByOrderIdOrderByCreatedAtDesc(orderId)
+                .orElseThrow(() -> new ValidationException("Event not found by OrderID."));
+    }
+
+    private Event findByTransactionId(String transactionId) {
+        return repository
+                .findTop1ByOrderIdOrderByCreatedAtDesc(transactionId)
+                .orElseThrow(() -> new ValidationException("Event not found by TransactionID."));
+    }
+
+    public void validationEmptyFilters(EventFilters filters) {
+        if(isEmpty(filters.getOrderId()) && isEmpty(filters.getTransactionId())){
+            throw new ValidationException("OrderID or TransactionID must be informed.");
+        }
+    }
 
     public Event save(Event event) {
         return repository.save(event);
---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Validation.java
@@ -0,0 +1,53 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "validation")
+public class Validation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String orderId;
+
+    @Column(nullable = false)
+    private String transactionId;
+
+    @Column(nullable = false)
+    private boolean success;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    // Função criada para que quando seja setado um set os valores por padrão ja serão setados sem a necessidade de implementar na mao.
+
+    @PrePersist
+    private void prePersist() {
+        var now = LocalDateTime.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+
+}
